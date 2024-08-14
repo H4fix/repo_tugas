994f3ec77fa665bb9c65f5e8b38681e1688aeac3
H4fix <tumbalksp@gmail.com>
2024-08-14 09:09:26 +0700

    menggubah file

--- hari ketiga/index.php
+++ hari ketiga/index.php
@@ -7,10 +7,18 @@ $sisaHasilBagi=$bilangan1%$bilangan2;
 
 //echo $bilangan1." modulus ".$bilangan2." = ".$sisaHasilBagi;
 
-echo $bilangan1;
-echo " modulus ";
-echo $bilangan2;
-echo " = ";
-echo $sisaHasilBagi;
+//echo $bilangan1;
+//echo " modulus ";
+//echo $bilangan2;
+//echo " = ";
+//echo $sisaHasilBagi;
 
 //hasil nya sama aja di nomor <8> ==> versi singkat kalau di nomor <10-14> versi panjang!!
+
+//operator perbandingan
+
+$bilA=7;
+$bilB="7";
+var_dump($bilA);
+var_dump($bilB);
+var_dump($bilA===$bilB);
