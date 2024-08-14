52a78ef7b8aa3a5f8ae5e4479707a729e293fb76
H4fix <tumbalksp@gmail.com>
2024-08-14 08:32:48 +0700

    membuat file baru

---
+++ hari ketiga/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$bilangan1=10;
+$bilangan2=3;
+
+$sisaHasilBagi=$bilangan1%$bilangan2;
+
+//echo $bilangan1." modulus ".$bilangan2." = ".$sisaHasilBagi;
+
+echo $bilangan1;
+echo " modulus ";
+echo $bilangan2;
+echo " = ";
+echo $sisaHasilBagi;
+
+//hasil nya sama aja di nomor <8> ==> versi singkat kalau di nomor <10-14> versi panjang!!
